32897dc45df949c725b8271183ee70ebcf8ae99c
josephesteva <joe.esteva@gmail.com>
2024-05-01 11:32:31 -0400

    Add tetris project link

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const dialogueData = {
 	exit: `Back to regular <a href="TODO" target="_blank">Portfolio</a>`,
 	pc: `This is where I do build my projects! You can see my code on <a href="https://github.com/josephesteva" target="_blank">Github</a>.`,
 	desk: `Here is a link to my <a href="TODO" target="_blank">resume</a>. You can also find me on <a href="https://linkedin/com/in/joseph-esteva" target="_blank">LinkedIn</a>.`,
-	game: `I enjoy gaming in my spare time. Some of my favorite series are The Legend of Zelda, Halo, and the Elder Scrolls!`,
+	game: `I built a <a href="https://josephesteva.github.io/tetris/" target="_blank">not very pretty tetris clone</a>. I enjoy gaming in my spare time. Some of my favorite series are The Legend of Zelda, Halo, and the Elder Scrolls!`,
 	books: `I love reading! Some of my favorite fiction books are <a href="https://www.goodreads.com/series/117100-red-rising-saga" target="_blank">Red Rising</a> and <a href="https://www.goodreads.com/series/45262-the-kingkiller-chronicle" target="_blank">The Name of the Wind</a>.
 	Some nonfiction books I've really enjoed are <a href="https://www.goodreads.com/book/show/41795733-range" target="_blank">Range</a> and <a href="https://www.goodreads.com/book/show/55539565-think-again" target="_blank">Think Again</a>.`,
 	bed: `Zzzzzzzzz`
