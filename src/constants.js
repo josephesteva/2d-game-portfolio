5dd195ffc0f89c4ec18fd5b85c072d84892fdc81
josephesteva <joe.esteva@gmail.com>
2024-05-13 11:37:18 -0400

    Update map and data with link to pokemon deck builder

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,8 @@ export const scaleFactor = 4;
 export const dialogueData = {
 	exit: `Back to regular <a href="TODO" target="_blank">Portfolio</a>`,
 	pc: `This is where I do build my projects! You can see my code on <a href="https://github.com/josephesteva" target="_blank">Github</a>.`,
-	desk: `Here is a link to my <a href="TODO" target="_blank">resume</a>. You can also find me on <a href="https://linkedin/com/in/joseph-esteva" target="_blank">LinkedIn</a>.`,
+	desk: `Here is a link to my <a href="https://drive.google.com/file/d/1fE6hBqKjZxz1MAQ9KWY_FV97C2doiWSQ/view?usp=sharing" target="_blank">resume</a>. You can also find me on <a href="https://linkedin/com/in/joseph-esteva" target="_blank">LinkedIn</a>.`,
+	pokemon: `I built a <a href="https://deckbuilder.onrender.com/" target="_blank">pokemon deck builder</a> as part of a group project! I have since updated it with improved navigation, styling, and functionality`,
 	game: `I built a <a href="https://josephesteva.github.io/tetris/" target="_blank">not very pretty tetris clone</a>. I enjoy gaming in my spare time. Some of my favorite series are The Legend of Zelda, Halo, and the Elder Scrolls!`,
 	books: `I love reading! Some of my favorite fiction books are <a href="https://www.goodreads.com/series/117100-red-rising-saga" target="_blank">Red Rising</a> and <a href="https://www.goodreads.com/series/45262-the-kingkiller-chronicle" target="_blank">The Name of the Wind</a>.
 	Some nonfiction books I've really enjoed are <a href="https://www.goodreads.com/book/show/41795733-range" target="_blank">Range</a> and <a href="https://www.goodreads.com/book/show/55539565-think-again" target="_blank">Think Again</a>.`,
